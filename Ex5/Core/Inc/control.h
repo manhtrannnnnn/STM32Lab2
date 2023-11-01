9aa7980f26a166236920eb8afec0e9aad40323c0
DESKTOP-9O3QD47\ASUS TUF <manh.tranduc@hcmut.edu.vn>
2023-11-01 21:49:23 +0700

    Ex5

---
+++ Ex5/Core/Inc/control.h
@@ -0,0 +1,16 @@
+#ifndef INC_CONTROL_H_
+#define INC_CONTROL_H_
+
+#include "main.h"
+
+extern const int MAX_LED;
+extern int index_led;
+extern int led_buffer[4];
+extern int hour, minute, second;
+void clear7SEG();
+void display7SEG(int num);
+void blinkLED();
+void update7SEG(int index);
+void updateClockBuffer();
+
+#endif /* INC_LED_H_ */
